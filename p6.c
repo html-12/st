d13c9d12ee4e5df1f3c27f53cd2ec8bd39e4f3bb
html-12 <157619671+html-12@users.noreply.github.com>
2024-08-08 22:36:36 +0530

    Update p6.c

--- p6.c
+++ p6.c
@@ -1,38 +1,48 @@
-# include<stdio.h> 
+#include <stdio.h>
+
 int main() 
 { 
-int n,i,key,low,high,mid,f=0,pos; 
-float a[10]; 
-printf("Enter the no. of elements\n"); 
-scanf("%d",&n); 
-printf("enter the elements in  the ascending order\n"); 
-for(i=0;i<n;i++) 
-scanf("%f",&a[i]); 
-printf("Enter the key element to be searched\n"); 
-scanf("%d",&key); 
-low=0; 
-high=n-1; 
-while(low<=high) 
-{ 
-mid=(low+high)/2; 
-if(key==a[mid]) 
-{ 
-f=1; 
-pos=mid; 
-break; 
-} 
-if(key>a[mid]) 
-low=mid+1; 
-else 
-high=mid-1; 
-} 
-if(f==1) 
-{ 
-printf("search successfull\n"); 
-printf("key %d is at location %d",key,pos+1); 
-} 
-else 
-/* 
-printf("search unsuccessfull\nkey not found"); 
-return 0;  
+    int n, i, low, high, mid, f = 0, pos; 
+    float key, a[10]; 
+
+    printf("Enter the number of elements\n"); 
+    scanf("%d", &n); 
+
+    printf("Enter the elements in ascending order\n"); 
+    for(i = 0; i < n; i++) 
+        scanf("%f", &a[i]); 
+
+    printf("Enter the key element to be searched\n"); 
+    scanf("%f", &key); 
+
+    low = 0; 
+    high = n - 1; 
+
+    while(low <= high) 
+    { 
+        mid = (low + high) / 2; 
+
+        if(key == a[mid]) 
+        { 
+            f = 1; 
+            pos = mid; 
+            break; 
+        } 
+        if(key > a[mid]) 
+            low = mid + 1; 
+        else 
+            high = mid - 1; 
+    } 
+
+    if(f == 1) 
+    { 
+        printf("Search successful\n"); 
+        printf("Key %.2f is at location %d\n", key, pos + 1); 
+    } 
+    else 
+    { 
+        printf("Search unsuccessful\nKey not found\n"); 
+    } 
+
+    return 0; 
 }
