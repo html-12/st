0c9aaac022fdce94d3b2a26f75d4b298b7f32b1d
html-12 <157619671+html-12@users.noreply.github.com>
2024-08-10 11:01:32 +0530

    Update p4.c

--- p4.c
+++ p4.c
@@ -1,36 +1,52 @@
-#include <stdio.h>
-
+#include<stdio.h> 
+#include<math.h> 
 int main() 
 { 
-    int a, b, c; 
-    printf("Enter 3 integers which are sides of a triangle: "); 
-    scanf("%d%d%d", &a, &b, &c); 
-
-    printf("Side A is %d\n", a); 
-    printf("Side B is %d\n", b); 
-    printf("Side C is %d\n", c); 
-
-    // Check if the given sides form a valid triangle
-    if ((a + b <= c) || (b + c <= a) || (a + c <= b)) 
-    { 
-        printf("Not a triangle\n"); 
-    } 
-    else 
-    { 
-        // Determine the type of triangle
-        if (a == b && b == c) 
-        { 
-            printf("Equilateral\n"); 
-        } 
-        else if (a == b || b == c || a == c) 
-        { 
-            printf("Isosceles\n"); 
-        } 
-        else 
-        { 
-            printf("Scalene\n"); 
-        } 
-    }
-
-    return 0; 
-}
+int a,b,c,match; 
+printf("Enter 3 integers which are sides of a triangle); 
+scanf("%d%d%d",&a,&b,&c); 
+printf("side A is %d\n",a); 
+printf("side B is %d\n",b); 
+printf("side C is %d\n",c); 
+match=0; 
+if(a==b) 
+match=match+1; 
+if(a==c) 
+match=match+2; 
+if(b==c) 
+match=match+3; 
+if(match==0) 
+{ 
+if((a+b)<=c) 
+printf("not a triangle\n"); 
+else if((b+c)<=a) 
+printf("not a traingle\n"); 
+else if((a+c)<+b) 
+printf("NOT A TRAINGLE\n"); 
+else 
+printf("scalene"); 
+} 
+if(match==1) 
+{ 
+if((a+c)<=b) 
+printf("not a triangle\n"); 
+else  
+printf("isosceles"); 
+} 
+else if(match==2) 
+{ 
+if((a+c)<=b) 
+printf("not a trianlge\n"); 
+else 
+printf("isosceles\n"); 
+} 
+else if(match==3) 
+{ 
+if((b+c)<=a) 
+printf("not a triangle\n"); 
+else 
+printf("isosceles\n"); 
+} 
+else 
+} 
+printf("Equilateral\n");
